a42a24af11b327dd5643fbf51ad3df63c5f26e83
Sam Margulies <friend@temple.edu>
2012-12-03 10:14:49 -0500

    Separate audio into its own tab
    And make the checkboxes on the same line in the add embed screen.

--- js/ensemble-video.js
+++ js/ensemble-video.js
@@ -6,6 +6,7 @@ jQuery(document).ready(function($) {
 		<div id='shortcode-type-header'>\
 		<ul>\
 			<li><a id='embed-video-link'>Add Video</a></li>\
+			<li><a id='embed-audio-link'>Add Audio</a></li>\
 			<li><a id='embed-destination-link'>Add Web Destination</a></li>\
 		</ul>\
 		</div>\
@@ -14,11 +15,11 @@ jQuery(document).ready(function($) {
 		<label>Content ID <input id='content-id-input' /></label></p>\
 		<p id='destination-id' class='for-web-destination'>\
 		<label>Destination ID <input id='destination-id-input' /></label></p>\
-		<p class='for-video for-web-destination'><label>Autoplay <input type='checkbox' id='autoplay' /></label></p>\
-		<p class='for-video for-web-destination'><label>Show Captions <input type='checkbox' id='show-captions' /></label></p>\
-		<p class='for-video'><label>Audio <input type='checkbox' id='audio' /></label></p>\
+		<p class='for-video for-web-destination'>\
+		<label><input type='checkbox' id='autoplay' /> Autoplay</label> &nbsp;&nbsp;&nbsp;\
+		<label><input type='checkbox' id='show-captions' /> Show Captions</label></p>\
 		<h3 class='for-web-destination'>Choose Layout</h3>\
-		<p class='for-web-destination'><label>Display Showcase <input type='checkbox' id='display-showcase' /></label></p>\
+		<p class='for-web-destination'><label><input type='checkbox' id='display-showcase' /> Display Showcase</label></p>\
 		<input class='button-primary' type=submit value='Add video' /> \
 		<input type='button' class='button' onclick='tb_remove(); return false;' value='Cancel' />\
 		</form>\
@@ -55,6 +56,15 @@ jQuery(document).ready(function($) {
 		
 	});
 	
+	$('#embed-audio-link').click(function(){
+	
+		$('#ensemble-video-inner a').removeClass('active');
+		$(this).addClass('active');
+		$('#ensemble-video-inner .for-web-destination').hide();
+		$('#ensemble-video-inner .for-video').show();
+		
+	});
+	
 	$('#embed-video-link').click();
 
 
@@ -68,11 +78,11 @@ jQuery(document).ready(function($) {
 	function generateEnsembleShortcode() {
 		var shortcode = "[ensemblevideo ";
 		
-		if( $('#ensemble-video-inner a#embed-video-link').is('.active') ) {
+		if( $('#ensemble-video-inner a#embed-video-link').is('.active') || $('#ensemble-video-inner a#embed-audio-link').is('.active') ) {
 			
 			shortcode += 'contentid=' + $('#content-id-input').val();
 			
-			if( $('#audio').is(':checked') ){
+			if( $('#ensemble-video-inner a#embed-audio-link').is('.active') ){
 				shortcode += ' audio=true';
 			}
 			
